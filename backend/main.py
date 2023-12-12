6afa9e00362493ab71075c7a3ac9fa51c0e08d8d
vsegda_vinovat <riabko97@gmail.com>
2023-12-12 22:04:33 +0200

    added new end_point

--- backend/main.py
+++ backend/main.py
@@ -1,4 +1,4 @@
-import fastapi as _fastapi
+import fastapi as _fastapi 
 import fastapi.security as _security
 
 import sqlalchemy.orm as _orm
@@ -11,8 +11,19 @@ app = _fastapi.FastAPI()
 
 @app.post('/api/users')
 async def create_user(user:_schemas.UserCreate, db: _orm.Session = _fastapi.Depends(_services.get_db)):
-    db_user = _services.get_user_by_email(user.email, db)
+    db_user = await _services.get_user_by_email(user.email, db)
     if db_user:
         raise _fastapi.HTTPException(status_code = 400, detail="Email already use")
     
     return await _services.create_user(user,db)
+
+
+@app.post('/api/token')
+async def generate_token(
+    form_data: _security.OAuth2PasswordRequestForm = _fastapi.Depends(), 
+    db: _orm.Session= _fastapi.Depends(_services.get_db)):
+    
+    user = await _services.authenticate_user(form_data.username, form_data.password,db)
+
+    if not user:
+        raise _fastapi.HTTPException(status_code=401, detail="Invalid Credentials")
